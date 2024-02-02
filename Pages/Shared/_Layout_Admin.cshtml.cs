d51c8345e7e6e1bd48ea2e3a2b83249ca6031895
Samed Salman <samed.salman@gmail.com>
2024-02-02 17:01:44 +0100

    Session cookie working properly for Administrators. Page layout shifts depending on what role is logged in.

---
+++ Pages/Shared/_Layout_Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fribergs_rentals_2.Pages.Shared
+{
+    public class _Layout_AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
